80fcddc8eae741abfc2dce4b3a7faa73ea77e5a4
Chineze Nwosu <chineze.nwosu@andela.com>
2018-07-11 13:20:26 +0100

    get and parse request

--- index.js
+++ index.js
@@ -5,12 +5,43 @@
 
 // Dependencies
 const http = require('http')
+const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
 
 // Variables
 const port = 8888
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World!\n')
+  // Get the url and parse
+  const parsedUrl = url.parse(req.url, true)
+
+  // Get the path
+  const path = parsedUrl.pathname
+
+  // Trim out slashes in path, get the method and query
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+  const method = req.method.toLowerCase()
+  const query = parsedUrl.query
+  const headers = req.headers
+  const decoder = new StringDecoder('utf-8')
+  let buffer = ''
+
+  req.on('data', (data) => {
+    buffer += decoder.write(data)
+  })
+
+  req.on('end', (data) => {
+    buffer += decoder.end()
+
+    console.log('Request received with body:', buffer)
+    console.log('Request received on path:', trimmedPath,)
+    console.log('Request received with method:', method)
+    console.log('Request received with query parameters:', query)
+    console.log('Request received with headers:', headers)
+
+    // Send the response
+    res.end('Hello World!\n')
+  })
 })
 
 // Listen on defined port
